afa2597057bddb023f0963738d0959d817ff8071
Marius Soutier <spybreak@gmx.net>
2011-08-27 19:23:08 +0200

    Twitter importer

---
+++ TwitterContactImporter/TwitterContactImporter/rb_main.rb
@@ -0,0 +1,24 @@
+#
+#  rb_main.rb
+#  TwitterContactImporter
+#
+#  Created by Marius Soutier on 27.08.11.
+#  Copyright (c) 2011 STAR Healthcare Management GmbH. All rights reserved.
+#
+
+# Loading the Cocoa framework. If you need to load more frameworks, you can
+# do that here too.
+framework 'Cocoa'
+framework "AddressBook"
+
+# Loading all the Ruby project files.
+main = File.basename(__FILE__, File.extname(__FILE__))
+dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
+  if path != main
+    require(path)
+  end
+end
+
+# Starting the Cocoa main loop.
+NSApplicationMain(0, nil)
